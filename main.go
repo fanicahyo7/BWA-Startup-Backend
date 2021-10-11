4d1cf6bf0522cb5fbe6fd10427780c6e975bde57
fanicahyo7 <fanidc7@gmail.com>
2021-10-11 19:17:39 +0700

    folder structur user

--- main.go
+++ main.go
@@ -1,20 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"bwastartup/user"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	// dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Koneksi Berhasil")
+	// var users []user.User
+	// length := len(users)
+	// fmt.Println(length)
+
+	// db.Find(&users)
+
+	// length = len(users)
+	// fmt.Println(length)
+
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// }
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Printf("Koneksi Berhasil")
+	var users []user.User
+	db.Find(&users)
+
+	c.JSON(http.StatusOK, users)
 }
