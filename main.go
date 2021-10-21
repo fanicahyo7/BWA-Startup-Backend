d22d4c39b1b9ab50b88e85d93cdfd71862035336
fanidc7 <fanidc7@gmail.com>
2021-10-21 15:44:32 +0700

    authmiddleware

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"bwastartup/auth"
 	"bwastartup/handler"
+	"bwastartup/helper"
 	"bwastartup/user"
 	"log"
+	"net/http"
+	"strings"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -39,7 +43,49 @@ func main() {
 	api.POST("users/", UserHandler.RegisterUser)
 	api.POST("sessions/", UserHandler.Login)
 	api.POST("checkemail/", UserHandler.CekEmailUser)
-	api.POST("avatars/", UserHandler.UploadAvatar)
+	api.POST("avatars/", authMiddleware(AuthService, UserService), UserHandler.UploadAvatar)
 	router.Run()
 
+}
+
+func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		if !strings.Contains(authHeader, "Bearer") {
+			response := helper.ApiResponse("unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		tokenString := ""
+		arrayToken := strings.Split(authHeader, " ")
+		if len(arrayToken) == 2 {
+			tokenString = arrayToken[1]
+		}
+
+		token, err := authService.ValidateToken(tokenString)
+		if err != nil {
+			response := helper.ApiResponse("unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		claim, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			response := helper.ApiResponse("unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		UserID := int(claim["user_id"].(float64))
+
+		user, err := userService.GetUserByID(UserID)
+		if err != nil {
+			response := helper.ApiResponse("unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		c.Set("currentUser", user)
+	}
 }
--- user/service.go
+++ user/service.go
@@ -11,6 +11,7 @@ type Service interface {
 	LoginUser(loginInput LoginInput) (User, error)
 	CekEmailUser(cekEmailInput CekEmailInput) (bool, error)
 	UpdateAvatar(ID int, locationFile string) (User, error)
+	GetUserByID(ID int) (User, error)
 }
 
 type service struct {
@@ -90,4 +91,17 @@ func (s *service) UpdateAvatar(ID int, locationFile string) (User, error) {
 	}
 
 	return updatedUser, nil
+}
+
+func (s *service) GetUserByID(ID int) (User, error) {
+	user, err := s.repository.FindByID(ID)
+	if err != nil {
+		return user, err
+	}
+
+	if user.ID == 0 {
+		return user, errors.New("no user found on that ID")
+	}
+
+	return user, nil
 }
