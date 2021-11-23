d2eb85090c54b73b2fb3781f8253065890ce4ecb
fanidc7 <fanidc7@gmail.com>
2021-11-23 14:13:06 +0700

    repository transaction

--- transaction/repository.go
+++ transaction/repository.go
@@ -9,6 +9,7 @@ type repository struct {
 type Repository interface {
 	GetByCampaignID(campaignID int) ([]Transaction, error)
 	GetByUserID(userID int) ([]Transaction, error)
+	Save(transaction Transaction) (Transaction, error)
 }
 
 func NewRepository(db *gorm.DB) *repository {
@@ -31,6 +32,15 @@ func (r *repository) GetByUserID(userID int) ([]Transaction, error) {
 	var transaction []Transaction
 	err := r.db.Preload("Campaign.CampaignImages", "campaign_images.is_primary = 1").Where("user_id = ?", userID).Order("id desc").Find(&transaction).Error
 
+	if err != nil {
+		return transaction, err
+	}
+
+	return transaction, nil
+}
+
+func (r *repository) Save(transaction Transaction) (Transaction, error) {
+	err := r.db.Create(&transaction).Error
 	if err != nil {
 		return transaction, err
 	}
